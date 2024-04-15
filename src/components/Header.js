43e7ad98a7b5f9cd031cf36186dc58c606c420e9
Ratul Ramchandani <theyogiwhocodes@gmail.com>
2024-04-15 12:43:37 +0530

    -- updated till video 6

---
+++ src/components/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { Nav, Navbar, Container, Row } from "react-bootstrap";
+
+function Header() {
+  return (
+    <header>
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary"
+        data-bs-theme="dark"
+        collapseOnSelect
+      >
+        <Container>
+          <Navbar.Brand href="/">ProShop</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto ">
+              <Nav.Link href="/cart">
+                <i className="fas fa-shopping-cart"></i>Cart
+              </Nav.Link>
+              <Nav.Link href="/login">
+                <i className="fas fa-user"></i>Login
+              </Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
+}
+
+export default Header;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+// import "./bootstrap.min-quartz.css";
+// import "./bootstrap.min-slate.css";
+import "./bootstrap.min-lux.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
